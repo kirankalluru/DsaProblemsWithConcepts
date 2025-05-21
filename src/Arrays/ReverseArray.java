d019c108ec973d19948719a5e85e18c98612b1af
kirankalluru <129094970+kirankalluru@users.noreply.github.com>
2025-05-21 13:51:15 +0530

    commit

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1,2,3,4,5,6,7,8};
         int k = 3;
         int[] nums = new int[arr.length];
         for(int i=0;i<k;i++){
